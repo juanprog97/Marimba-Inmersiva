49b1f5785c9f158b284be29603be8fff20a52313
JMC TOR <juancard190@gmail.com>
2019-10-14 22:43:46 -0500

    Agregando Nuevo espacio

---
+++ ProyectoMarimba/Proyecto Marimba/Assets/Text.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Text : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject text = new GameObject();
+        TextMesh t = text.AddComponent<TextMesh>();
+        Color fond = new Color(0.3f, 0.4f, 0.6f);
+        t.text = "Prueba";
+        t.fontSize = 120;
+        t.color = fond;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
