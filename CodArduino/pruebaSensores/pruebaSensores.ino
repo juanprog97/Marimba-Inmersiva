62ff68949f6223baf8ca13cb28e42016ce31ac43
JMC TOR <juancard190@gmail.com>
2020-03-11 22:09:52 -0500

    Cod Microcontroller to test comunication sensor to unity

--- CodArduino/pruebaSensores/pruebaSensores.ino
+++ CodArduino/pruebaSensores/pruebaSensores.ino
@@ -1,4 +1,7 @@
-int p;
+int p1;
+int p2;
+int p3;
+String trama;
 void setup() {
   Serial.begin(9600);
   
@@ -6,7 +9,35 @@ void setup() {
 }
 
 void loop() {
-  p = analogRead(A0);
-  Serial.println(p);
-  delay(50);
+  trama = "";
+  p1 = analogRead(A2); //Sube
+  p2 = analogRead(A0);//Baja
+  p3 = analogRead(A1); //UNDo
+  if(p1 <=600){
+    trama+="1";
+    delay(20);
+  }
+  else{
+    trama+="0";
+    
+  }
+  if(p2 <=600){
+     trama+="1";
+     delay(20);
+  }
+  else{
+    trama+="0";
+    
+  }
+  if(p3<=600){
+     trama+="1";
+     delay(20);
+  }
+  else{
+    trama+="0";
+    
+  }
+  delay(100);
+  Serial.println(trama);
+  
 }
