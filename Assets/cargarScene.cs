9166eed23fca6abd2c118730eb9f4bcf9172a8e0
JMC TOR <juancard190@gmail.com>
2020-11-11 00:30:23 -0500

    Posicionado elementos en el recuadro

--- Assets/cargarScene.cs
+++ Assets/cargarScene.cs
@@ -1,24 +1,40 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
 
+
 public class cargarScene : MonoBehaviour
 {
-    private string UrlFirebase = "gs://quickstart-1595792293378.appspot.com/AssetsBundles/MaterialMultimedia/";
+ 
+
+    private string UrlFirebase = "gs://quickstart-1595792293378.appspot.com/AssetsBundles/MaterialMultimedia/historia/";
     public GameObject pantallaDescarga;
     public GameObject pantallaElementos;
     public Animation first;
     public Animation second;
+    public GameObject itemTexto;
     List<RenderTexture> renderTexture;
     public GameObject audioElement;
+    public GameObject titleScene;
+    public GameObject videoElement;
+    private code_ui_history.Escena EscenaSeleccionada;
+    private string AssetName;
 
-    private string AssetName = "prueba1";
+   
 
-    private List<string> listadoObjeto = new List<string> { "fotoGaitan", "Gaitan", "Cheese", "discursoGaitan", "videoExample"};
-    private List<string> tipoObjeto = new List<string> { "image", "image", "3d", "audio", "video"};
+
+    public void setEscenaSeleccionada(code_ui_history.Escena escena)
+    {
+        this.EscenaSeleccionada = escena;
+        this.AssetName = escena.AssetName;
+
+    }
+
+
 
     [Obsolete]
     void distribuirElementos(AssetBundle asset)
@@ -28,53 +44,77 @@ public class cargarScene : MonoBehaviour
         second.Stop();
         pantallaDescarga.SetActive(false);
         pantallaElementos.SetActive(true);
-        renderTexture = new List<RenderTexture>();
-        for (int i = 0; i< tipoObjeto.Count; i++)
+        titleScene.GetComponent<Text>().text = EscenaSeleccionada.Title;
+        for (int i = 0; i< EscenaSeleccionada.Materials.Count; i++)
         {
-            string elem = tipoObjeto[i];
-
+            string elem = EscenaSeleccionada.Materials[i].Type;
+            code_ui_history.Material material = EscenaSeleccionada.Materials[i];
+            Debug.Log(elem);
             if (elem == "image")
             {
-                elemTmp = new GameObject(listadoObjeto[i]);
+                elemTmp = new GameObject(material.Name);
                 elemTmp.AddComponent<Image>();
-                elemTmp.GetComponent<Image>().sprite = asset.LoadAsset<Sprite>(listadoObjeto[i]);
+                elemTmp.GetComponent<Image>().sprite = asset.LoadAsset<Sprite>(material.Name);
                 //Posicion y Escala
                 elemTmp.transform.parent = pantallaElementos.transform.GetComponent<Transform>();
                 elemTmp.transform.localScale = new Vector3(1, 1, 1);
-
+                elemTmp.transform.rotation.Set(0, 0, -90, 0);
+                elemTmp.GetComponent<RectTransform>().sizeDelta = new Vector2(material.Propierty.Dimension.Width,
+                    material.Propierty.Dimension.Height);
+                elemTmp.GetComponent<RectTransform>().localPosition = new Vector3(material.Propierty.Position.PosX,
+                    material.Propierty.Position.PosY, material.Propierty.Position.PosZ);
+               
             }
             else if (elem == "3d")
             {
 
-                elemTmp = Instantiate(asset.LoadAsset<GameObject>(listadoObjeto[i]),
+                elemTmp = Instantiate(asset.LoadAsset<GameObject>(material.Name),
                     pantallaElementos.transform.GetComponent<Transform>());
                 //Posicion y Escala
             }
             else if (elem == "audio")
             {
+                
                 audioElement.transform.FindChild("sample").GetComponent<AudioSource>().clip =
-                    asset.LoadAsset<AudioClip>(listadoObjeto[i]);
+                    asset.LoadAsset<AudioClip>(material.Name);
+                audioElement.SetActive(true);
+                audioElement.transform.FindChild("Titulo").GetComponent<Text>().text = material.Description;
+                audioElement.transform.FindChild("Titulo").GetComponent<RectTransform>().sizeDelta = 
+                    new Vector2(material.Propierty.Dimension.Width,
+                   material.Propierty.Dimension.Height);
+                audioElement.transform.FindChild("Titulo").GetComponent<RectTransform>().localPosition =
+                    new Vector3(material.Propierty.Position.PosX,
+                    material.Propierty.Position.PosY, material.Propierty.Position.PosZ);
                 audioElement.transform.FindChild("sample").GetComponent<AudioSource>().Play();
+            }
+            else if(elem == "text")
+            {
+
+                elemTmp = Instantiate(itemTexto, pantallaElementos.transform.GetComponent<Transform>());
+                elemTmp.GetComponent<RectTransform>().sizeDelta = new Vector2(material.Propierty.Dimension.Width,
+                   material.Propierty.Dimension.Height);
+                elemTmp.GetComponent<RectTransform>().localPosition = new Vector3(material.Propierty.Position.PosX,
+                    material.Propierty.Position.PosY, material.Propierty.Position.PosZ);
+                elemTmp.transform.localScale = new Vector3(1, 1, 1);
+                elemTmp.GetComponent<Text>().text = material.Description;
+                elemTmp.transform.name = "texto" + i.ToString();
+                elemTmp.GetComponent<Text>().fontSize = Convert.ToInt32(material.Propierty.FontSize);
+
+
+
             }
             else if (elem == "video")
             {
-                RenderTexture renderTe = new RenderTexture(1280, 720, 1);
-                renderTexture.Add(renderTe);
-                elemTmp = new GameObject(listadoObjeto[i]);
-                GameObject video = new GameObject("videoDireccion");
-                GameObject imageTextura = new GameObject("imageRaw");
-                video.AddComponent<VideoPlayer>();
-                video.GetComponent<VideoPlayer>().clip = asset.LoadAsset<VideoClip>(listadoObjeto[i]);
-                video.GetComponent<VideoPlayer>().renderMode = VideoRenderMode.RenderTexture;
-                video.GetComponent<VideoPlayer>().targetTexture = renderTe;
-                imageTextura.AddComponent<RawImage>().texture = renderTe;
-
-                video.transform.parent = elemTmp.GetComponent<Transform>();
-                imageTextura.transform.parent = elemTmp.GetComponent<Transform>();
-                //Posicion y Escala
-
-                elemTmp.transform.parent = pantallaElementos.transform.GetComponent<Transform>();
-                elemTmp.transform.localScale = new Vector3(1, 1, 1);
+                elemTmp = Instantiate(videoElement, pantallaElementos.transform.GetComponent<Transform>());
+                elemTmp.transform.FindChild("videoSample").GetComponent<VideoPlayer>().clip  = 
+                    asset.LoadAsset<VideoClip>(material.Name);
+                elemTmp.name = "video" + i.ToString();
+                elemTmp.transform.FindChild("sample").GetComponent<RectTransform>().sizeDelta = new Vector2(material.Propierty.Dimension.Width,
+                   material.Propierty.Dimension.Height);
+                elemTmp.transform.FindChild("sample").GetComponent<RectTransform>().localPosition = new Vector3(material.Propierty.Position.PosX,
+                    material.Propierty.Position.PosY, material.Propierty.Position.PosZ);
+                elemTmp.transform.FindChild("sample").transform.localScale = new Vector3(1, 1, 1);
+                elemTmp.transform.FindChild("videoSample").GetComponent<VideoPlayer>().Play();
             }
 
             //Desplegar Texto y titulo
@@ -95,7 +135,7 @@ public class cargarScene : MonoBehaviour
         while (!Caching.ready)
             yield return null;
 
-        WWW www = WWW.LoadFromCacheOrDownload(task.Result.ToString(), 2);
+        WWW www = WWW.LoadFromCacheOrDownload(task.Result.ToString(), this.EscenaSeleccionada.Version);
 
 
         while (!www.isDone)
@@ -125,8 +165,11 @@ public class cargarScene : MonoBehaviour
         }
     }
 
-
-    void Start()
+    void OnDisable()
+    {
+        Debug.Log("borrarElementos");
+    }
+    void OnEnable()
     {
         iniciarDescargar();
     }
--- Assets/code_ui_history.cs
+++ Assets/code_ui_history.cs
@@ -13,52 +13,73 @@ using UnityEditor;
 public class code_ui_history : MonoBehaviour
 {
 
-    public partial class Escenas
-    {
-        [JsonProperty("historia")]
-        public List<Historia> Historia { get; set; }
-    }
-
     public partial class Historia
     {
-        [JsonProperty("title")]
-        public string Title { get; set; }
+        [JsonProperty("Escenas")]
+        public List<Escena> Escenas { get; set; }
+    }
 
-        [JsonProperty("time")]
-        public long Time { get; set; }
-
+    public partial class Escena
+    {
         [JsonProperty("AssetName")]
         public string AssetName { get; set; }
 
-        [JsonProperty("info")]
-        public string Info { get; set; }
-
         [JsonProperty("materials")]
         public List<Material> Materials { get; set; }
+
+        [JsonProperty("order")]
+        public long Order { get; set; }
+
+        [JsonProperty("time")]
+        public string Time { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("version")]
+        public int Version { get; set; }
     }
 
     public partial class Material
     {
-        [JsonProperty("version")]
-        public long Version { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("propierty")]
+        public Propierty Propierty { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("typeAnimation")]
+        public string TypeAnimation { get; set; }
+    }
+
+    public partial class Propierty
+    {
+        [JsonProperty("dimension")]
+        public Dimension Dimension { get; set; }
+
+        [JsonProperty("fontSize")]
+        public long FontSize { get; set; }
 
         [JsonProperty("position")]
         public Position Position { get; set; }
 
         [JsonProperty("scale")]
         public Scale Scale { get; set; }
+    }
 
-        [JsonProperty("type")]
-        public string Type { get; set; }
+    public partial class Dimension
+    {
+        [JsonProperty("height")]
+        public long Height { get; set; }
 
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("description")]
-        public string Description { get; set; }
-
-        [JsonProperty("typeAnimation")]
-        public string TypeAnimation { get; set; }
+        [JsonProperty("width")]
+        public long Width { get; set; }
     }
 
     public partial class Position
@@ -85,25 +106,37 @@ public class code_ui_history : MonoBehaviour
         public long ScaleZ { get; set; }
     }
 
+
     private const string projectId = "quickstart-1595792293378";
     private static readonly string databaseURL = $"https://{projectId}.firebaseio.com";
-    public GameObject boton_izquierdo;
-    public GameObject boton_derecho;
+    public GameObject menuCatalogo;
    // private componentBluetooth escuchaComando;
     public GameObject ContentTitulos;
     public GameObject itemTitulos;
     public Text TextoTiempo;
     public Slider slideTiempo;
     int estadoPeriodo = 0;
-    private Escenas DatosEscenas;
+    private Historia DatosEscenas;
+    public GameObject cargarScene;
+    public List<Tuple<long,int>> ordenPeriodo ;
 
 
 
-
+    public void ordenarPeriodos()
+    {
+        ordenPeriodo = new List<Tuple<long, int>>();
+        for (int i = 0; i < this.DatosEscenas.Escenas.Count; i++)
+        {
+            Tuple<long, int> data = new Tuple<long, int>(this.DatosEscenas.Escenas[i].Order,i);
+            this.ordenPeriodo.Add(data);
+        }
+        this.ordenPeriodo.Sort();
+        Debug.Log(ordenPeriodo[0].Item1);
+    }
 
     public IEnumerator consultarDatos()
     {
-        UnityWebRequest www = UnityWebRequest.Get($"{databaseURL}/DataGame/Multimedia.json");
+        UnityWebRequest www = UnityWebRequest.Get($"{databaseURL}/DataGame/Multimedia/Historia.json");
         yield return www.SendWebRequest();
         if (www.isNetworkError || www.isHttpError)
         {
@@ -115,7 +148,9 @@ public class code_ui_history : MonoBehaviour
         {
             try
             {
-                this.DatosEscenas = JsonConvert.DeserializeObject<Escenas>(www.downloadHandler.text);
+                Debug.Log(www.downloadHandler.text);
+                this.DatosEscenas = JsonConvert.DeserializeObject<Historia>(www.downloadHandler.text);
+                ordenarPeriodos();
                 desplegarPeriodos();
             }
             catch
@@ -132,21 +167,21 @@ public class code_ui_history : MonoBehaviour
     void desplegarPeriodos()
     {
 
-        UnityEngine.XR.XRSettings.enabled = true;
-        //componentBluetooth.Instance.seTocoBoton += Instance_seTocoBoton;
+       // UnityEngine.XR.XRSettings.enabled = true;
+       // componentBluetooth.Instance.seTocoBoton += Instance_seTocoBoton;
         GameObject item = null ;
         this.slideTiempo.minValue = 0;
-        this.slideTiempo.maxValue = this.DatosEscenas.Historia.Count-1 ;
+        this.slideTiempo.maxValue = this.DatosEscenas.Escenas.Count-1 ;
         this.slideTiempo.value = 0;
-        for (int i = 0; i< this.DatosEscenas.Historia.Count; i++)
+        for (int i = 0; i< ordenPeriodo.Count ; i++)
         {
             item = Instantiate(itemTitulos);
-            item.GetComponent<Text>().text = this.DatosEscenas.Historia[i].Title;
+            item.GetComponent<Text>().text = this.DatosEscenas.Escenas[ordenPeriodo[i].Item2].Title;
             item.transform.parent = ContentTitulos.transform;
             item.transform.localScale = new Vector3(1, 1, 1);
             item.transform.LeanSetPosZ(0);
         }
-        TextoTiempo.GetComponent<Text>().text = this.DatosEscenas.Historia[this.estadoPeriodo].Time.ToString();
+        TextoTiempo.GetComponent<Text>().text = this.DatosEscenas.Escenas[ordenPeriodo[this.estadoPeriodo].Item2].Time.ToString();
 
 
     }
@@ -177,8 +212,9 @@ public class code_ui_history : MonoBehaviour
             if (this.estadoPeriodo != 0)
             {
                 this.estadoPeriodo--;
-                LeanTween.moveLocalX(ContentTitulos, ContentTitulos.transform.localPosition.x + 890, 0.25f).setEaseOutCubic(); ;
-                TextoTiempo.GetComponent<Text>().text = this.DatosEscenas.Historia[this.estadoPeriodo].Time.ToString();
+                LeanTween.moveLocalX(ContentTitulos, ContentTitulos.transform.localPosition.x + 847.5f, 0.25f).setEaseOutCubic(); ;
+                TextoTiempo.GetComponent<Text>().text = this.DatosEscenas.Escenas[ordenPeriodo[this.estadoPeriodo].Item2].
+                    Time.ToString();
                 this.slideTiempo.value = this.estadoPeriodo;
 
             }
@@ -190,21 +226,70 @@ public class code_ui_history : MonoBehaviour
     {
         if (LeanTween.tweensRunning == 0)
         {
-            if (this.estadoPeriodo < this.DatosEscenas.Historia.Count - 1)
+            if (this.estadoPeriodo < this.DatosEscenas.Escenas.Count - 1)
             {
                 this.estadoPeriodo++;
-                LeanTween.moveLocalX(ContentTitulos, ContentTitulos.transform.localPosition.x - 890, 0.25f).setEaseOutCubic();
-                TextoTiempo.GetComponent<Text>().text = this.DatosEscenas.Historia[this.estadoPeriodo].Time.ToString();
+                LeanTween.moveLocalX(ContentTitulos, ContentTitulos.transform.localPosition.x - 847.5f, 0.25f).setEaseOutCubic();
+                TextoTiempo.GetComponent<Text>().text = this.DatosEscenas.Escenas[ordenPeriodo[this.estadoPeriodo].Item2].
+                    Time.ToString();
                 this.slideTiempo.value = this.estadoPeriodo;
             }
         }
-       
-
+  
     }
+
+    void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.isKey)
+        {
+            if (e.keyCode == KeyCode.Space)
+            {
+                cargarScene.GetComponent<cargarScene>().setEscenaSeleccionada(this.DatosEscenas.Escenas[ordenPeriodo[this.estadoPeriodo].Item2]);
+                cargarScene.SetActive(true);
+                menuCatalogo.SetActive(false);
+                this.gameObject.SetActive(false);
+            }
+            if (e.keyCode == KeyCode.RightArrow)
+            {
+                if (LeanTween.tweensRunning == 0)
+                {
+                    if (this.estadoPeriodo < this.DatosEscenas.Escenas.Count - 1)
+                    {
+                        this.estadoPeriodo++;
+                        LeanTween.moveLocalX(ContentTitulos, ContentTitulos.transform.localPosition.x - 847.5f, 0.25f).setEaseOutCubic();
+                        TextoTiempo.GetComponent<Text>().text = this.DatosEscenas.Escenas[ordenPeriodo[this.estadoPeriodo].Item2].
+                            Time.ToString();
+                        this.slideTiempo.value = this.estadoPeriodo;
+                    }
+                }
+
+            }
+            if (e.keyCode == KeyCode.LeftArrow)
+            {
+                if (LeanTween.tweensRunning == 0)
+                {
+                    if (this.estadoPeriodo != 0)
+                    {
+                        this.estadoPeriodo--;
+                        LeanTween.moveLocalX(ContentTitulos, ContentTitulos.transform.localPosition.x + 847.5f, 0.25f).setEaseOutCubic(); ;
+                        TextoTiempo.GetComponent<Text>().text = this.DatosEscenas.Escenas[ordenPeriodo[this.estadoPeriodo].Item2].
+                            Time.ToString();
+                        this.slideTiempo.value = this.estadoPeriodo;
+
+                    }
+                }
+
+            }
+
+        }
+    }
+
+
 
     private void okBoton()
     {
-        Debug.Log("jej");
+        
     }
 
     private void regresarBoton()
@@ -214,7 +299,7 @@ public class code_ui_history : MonoBehaviour
 
     private void salir()
     {
-        UnityEngine.XR.XRSettings.enabled = false;
+     //   UnityEngine.XR.XRSettings.enabled = false;
         SceneManager.LoadScene("MenuMultimediaInteractivo");
     }
 
@@ -250,10 +335,10 @@ public class code_ui_history : MonoBehaviour
         }
     }
 
-    void OnDisable()
+   /* void OnDisable()
     {
-      //  componentBluetooth.Instance.seTocoBoton += Instance_seTocoBoton;
-    }
+        componentBluetooth.Instance.seTocoBoton += Instance_seTocoBoton;
+    }*/
 
     // Update is called once per frame
 
